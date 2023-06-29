76ca2bbd22d6bee1142bab4272c59708cdbfc2ce
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-06-29 13:51:25 -0300

    first commit

---
+++ src/components/button/Index.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from "react"
+
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+interface botaoProps{
+    children: ReactElement | string
+}
+
+export default function Botao( {children}:botaoProps){
+    return (
+        <button className="w-full md:max-w-[22.8rem] py-5 rounded font-bold text-white text-[26px] bg-purple-700 shadow-[0px_0px_20px_#000] hover:bg-[#028986] hover:shadow-[0px_0px_20px_#fff] transition-all duration-200">
+            
+            <a href="https://wa.me/5555349993535" target='_blank'> <WhatsAppIcon className='fill-white w-[40px] h-[40px] animate-bounce'/> </a>
+            {children}
+        </button>
+    )
+}
